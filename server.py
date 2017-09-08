c3388a4ef214c6bb9973e1a3304b0fcc281adb09
Patrick <kaorti@gmail.com>
2017-09-08 14:34:41 -0700

    number guessing game in flask

---
+++ server.py
@@ -0,0 +1,35 @@
+from random import randint
+from flask import Flask, render_template, request, session, redirect, url_for
+
+
+app = Flask(__name__)
+app.secret_key = 'supersecret'
+
+
+@app.route('/')
+def index():
+    if 'target' not in session:
+        session['target'] = randint(1, 100)
+    return render_template('index.html')
+
+
+@app.route('/answer', methods=['POST'])
+def answer():
+    # import pdb; pdb.set_trace()
+    if 'restart' in request.form:
+        session.pop('target')
+        return redirect(url_for('index'))
+    answer = int(request.form['guess'])
+    if answer > session['target']:
+        hint = "Too High! Guess again!"
+        right = False
+    elif answer < session['target']:
+        hint = "Too Low! Guess again!"
+        right = False
+    else:
+        hint = "Exactly Right! You win!"
+        right = True
+    return render_template('index.html', hint=hint, right=right)
+
+
+app.run(debug=True)
